db374399e360cb57dc107c43c72de129c76dd553
sarvagnakadiya <sarvagnakadiya1@gmail.com>
2024-01-31 17:44:11 +0530

    app.jsx bug solved

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import Login from "./components/Login";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 import Footer from "./components/Footer";
-import login from "./components/login";
 
 function App() {
   return (
